ded775f1fd765d1adb3d0cff31734214c03cf193
jmlee <j0n9m1n1@gmail.com>
2023-03-27 23:53:38 +0900

    tab

--- main.py
+++ main.py
@@ -22,62 +22,86 @@ class MainWindow(QMainWindow):
 
     def __init__(self):
         super(MainWindow, self).__init__()
-        # layout = QVBoxLayout()
-        self.ui()
+
+        self.ui_init()
+        self.ui_setup()
         
-    def ui(self):
-        
+    def ui_init(self):
+        self.tab_widget = QTabWidget()
+
+        self.tab = QWidget()
+
         self.grid_layout = QGridLayout()
+
         self.table_widget = QTableWidget()
-        
-        # button = QPushButton("TEST")
+        self.line_edit_dcm_path = QLineEdit()
+        self.button_confirm_path = QPushButton()
+
+        self.tab2 = QWidget()
+        self.grid_layout2 = QGridLayout()
+        self.list_widget_tag = QListWidget()
+        self.line_edit_tag = QLineEdit()
+        self.button_tag_add = QPushButton()
+        self.button_tag_delete = QPushButton()
+        self.button_tag_up = QPushButton()
+        self.button_tag_down = QPushButton()
+
+
+    def ui_setup(self):
+        self.tab_widget.addTab(self.tab, 'Tab')
 
         self.table_widget.setColumnCount(len(list_tag))
         self.table_widget.setHorizontalHeaderLabels(list_tag)
         self.table_widget.setRowCount(50)
 
-        self.line_edit_dcm_path = QLineEdit(r"G:\아이디\SERIES1")
-        self.button_confirm_path = QPushButton("GET")
+        self.line_edit_dcm_path.setText(r"G:\아이디\SERIES1")
+
+        self.button_confirm_path.setText("Get")
         self.button_confirm_path.clicked.connect(self.button_clicked_confirm_path)
-        
+
         self.grid_layout.addWidget(self.line_edit_dcm_path, 0, 0)
         self.grid_layout.addWidget(self.button_confirm_path, 0, 1)
-        
-
-
-        # layout.addWidget(text_edit_dcm_path)
         self.grid_layout.addWidget(self.table_widget, 1, 0, 1, 0)
-        # layout.addWidget(table_widget)
-        # layout.addWidget(button)
 
-        # grid_layout.addLayout(layout, 0, 0)
-        self.widget = QWidget()
-        self.widget.setLayout(self.grid_layout)
+        self.tab.setLayout(self.grid_layout)
+
+        self.tab_widget.addTab(self.tab2, 'Tab2')
+        self.list_widget_tag.addItems(list_tag)
+
+        self.button_tag_add.setText("Add")
+        self.button_tag_delete.setText("Delete")
+        self.button_tag_up.setText("Up")
+        self.button_tag_down.setText("Down")
+
+        self.grid_layout2.addWidget(self.list_widget_tag, 0, 0)
+        self.grid_layout2.addWidget(self.line_edit_tag, 0, 1)
+
+        self.grid_layout2.addWidget(self.button_tag_add, 1, 1)
+        self.grid_layout2.addWidget(self.button_tag_delete, 2, 1)
+        self.grid_layout2.addWidget(self.button_tag_up, 3, 1)
+        self.grid_layout2.addWidget(self.button_tag_down, 4, 1)
         
+        self.tab2.setLayout(self.grid_layout2)
+
         self.setFixedWidth(800)
         self.setFixedHeight(600)
-        
-        self.setCentralWidget(self.widget)
-        self.setWindowTitle("My App")
-        # line_edit_dcm_path.toPlainText()
+        self.setCentralWidget(self.tab_widget)
+        self.setWindowTitle("DICOM Header Editor")
 
     def button_clicked_confirm_path(self):
-        # print(path)
-        # sender = self.sender()
         input_path = self.line_edit_dcm_path.text()
         print(input_path)
         self.insert_file_list_to_table_widget(input_path)
         
     def insert_file_list_to_table_widget(self, path):
         dict_files = os.listdir(path)
-        print(dict_files)
+        # print(dict_files)
         for i, file_name in enumerate(dict_files):
             file_path = os.path.join(path, file_name)
             ds = dcmread(file_path, stop_before_pixels=True, force=True)
             # print(ds)
             for j, tag in enumerate(list_tag):
                 self.table_widget.setItem(i, j, QTableWidgetItem(str(ds[list_tag[j]].value)))
-        
                 # print(ds[list_tag[j]].value)
         self.table_widget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         
